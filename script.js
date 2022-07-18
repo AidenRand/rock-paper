a375abfc65957c90eb811da713cf66df52ae4b28
Cortrax <aidenrphotos@gmail.com>
2022-07-18 15:24:07 -0400

    add refresh alert at end of 5 round

--- script.js
+++ script.js
@@ -17,33 +17,55 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let winner;
   if (playerSelection === "rock" && computerSelection === "rock") {
-    return (winner = "it's a tie");
+    return (winner = "There is no clear winner");
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return (winner = "it's a tie");
-  } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    return (winner = "it's a tie");
+    return (winner = "Humans and A.I are at a standstill");
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return (winner = "There is no winner");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return (winner = "you win");
+    userScore++;
+    return (winner = "You have defeated the robot overlords");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return (winner = "you win");
+    userScore++;
+    return (winner = "You have beaten the A.I");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return (winner = "you win");
+    userScore++;
+    return (winner = "You win this round");
   } else if (playerSelection === " scissors" && computerSelection === "rock") {
-    return (winner = "you lose");
+    compScore++;
+    return (winner = "The Robots win");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return (winner = "you lose");
+    compScore++;
+    return (winner = "The A.I overlord wins");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return (winner = "you lose");
+    compScore++;
+    return (winner = "A.I wins");
   }
 }
 
 function game() {
-  for (i = 0; i < 5; i++) {
-    const playerSelection = prompt("rock paper or scissors");
+  for (i = 0; i < 6; i++) {
+    const playerSelection = prompt("rock paper or scissors").toLowerCase();
     const computerSelection = getComputerChoice();
     console.log(playerSelection);
     console.log(computerSelection);
     console.log(playRound(playerSelection, computerSelection));
+    console.log(userScore, compScore);
+  }
+
+  if (userScore === compScore) {
+    console.log("there is no clear winner");
+  } else if (userScore > compScore) {
+    console.log("the humans will live to fight another day");
+  } else if (userScore < compScore) {
+    console.log("you have failed us all");
+  }
+
+  if (i === 6) {
+    alert("Refresh to fight again");
   }
 }
 game();
